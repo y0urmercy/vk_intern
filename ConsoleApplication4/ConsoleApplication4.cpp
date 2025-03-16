255893336242415ca6b81f66e86b4a18bf01546a
simka <lena.sherstuk2006@mail.ru>
2025-03-16 09:53:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int start;
+    int nVert, nEdges;
+    int a, b;
+    vector<vector<int>> graph(nVert);
+
+    ifstream infile("graph.txt");
+    infile >> nVert >> nEdges;
+
+    for (int i = 0; i < nEdges; i++)
+    {
+        infile >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    infile >> start;
+    infile.close();
+
+    vector<int> dist(nVert, -1);
+    dist[start] = 0;
+
+    queue<int> q;
+    q.push(start);
+
+    while (!q.empty())
+    {
+        int curr = q.front();
+        q.pop();
+
+        for (int adj : graph[curr]) 
+        {
+            if (dist[adj] == -1)
+            {
+                dist[adj] = dist[curr] + 1;
+                q.push(adj);
+            }
+        }
+    }
+
+    for (int i = 0; i < nVert; i++)
+    {
+        cout << dist[i] << endl;
+    }
+}
+
